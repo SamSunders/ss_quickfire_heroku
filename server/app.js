d749bb0a4a2fb3d4e8450451166d4750d556b503
Samantha Sunders <samanthasunders@gmail.com>
2015-07-29 11:49:31 -0500

    blah

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "./public/views/index.html"));
+    res.sendFile(path.join(__dirname, "../assets/scripts/styles.css"));
 });
 
 app.listen(app.get('port'), function(){
